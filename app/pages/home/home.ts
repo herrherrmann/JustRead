ac5adb7c12f5db0c5f73ff756f17a8484e12e0fe
Sebastian Herrmann <sebastian@herrherrmann.net>
2016-09-26 00:17:47 +0200

    fix: use 'en_US' locale for now

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,27 +1,33 @@
 import { Component, Input } from '@angular/core';
 import { Http } from '@angular/http';
 import { NavController } from 'ionic-angular';
+import { Globalization } from 'ionic-native';
 import { TitleResult } from './title-result/title-result';
+
 
 @Component({
   templateUrl: 'build/pages/home/home.html',
   directives: [TitleResult],
 })
 export class HomePage {
+  locale: string;
   searchTerm: string;
   titles: any;
   searching: boolean;
 
   constructor(public http: Http) {
     this.search();
+    // Globalization.getLocaleName()
+    //   .then(locale => this.locale = locale)
+    //   .catch(error => {});
   }
 
   search() {
-    console.log('search: ', this.searchTerm);
+    const locale = this.locale || 'en_US';
     this.titles = [];
     this.searching = true;
     this.http
-      .post('https://api.justwatch.com/titles/de_DE/popular', {
+      .post(`https://api.justwatch.com/titles/${locale}/popular`, {
         content_types: ['show', 'movie'],
         page: 1,
         page_size: 20,
